2c54f25a4d304bc86d269f1d764aff1d142eaf71
Astrid2601 <astridlilibeth3@gmail.com>
2024-03-15 17:24:20 -0500

    Restablecer contraseña en el conn.js

--- Model/conn.js
+++ Model/conn.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const conn = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"12345",
+    password:"",
     database:"Db_Cordination"
 })
 
