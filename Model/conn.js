72419dcad99ade3528b91f941743229cf8c61bd7
herreracuesta <jvherrera66@misena.edu.co>
2024-06-24 13:00:35 -0500

    Se sube la configuracion de vercel en el archivo conn y el certificado

--- Model/conn.js
+++ Model/conn.js
@@ -1,24 +1,47 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
+
+// Asegúrate de especificar la ruta correcta al certificado CA
+const caCertPath = path.join(__dirname, '../certs/DigiCertGlobalRootCA.crt.pem');
+
+let caCert;
+try {
+    caCert = fs.readFileSync(caCertPath);
+} catch (error) {
+    console.error(`Error al leer el certificado: ${error.message}`);
+    process.exit(1);
+}
 
 const conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"12345",
-    database:"Db_Cordination"
-})
-
-conn.connect((error)=>{
-    if(error){
-        console.error(`Error al conectarse a la base de datos: ${error}`)
-        return
+    host: "admj2024.mysql.database.azure.com",
+    user: "Proyecto_Grupal",
+    password: "NV2024cordination",
+    database: "Db_Cordination",
+    port: 3306,
+    ssl: {
+        ca: caCert // Se especifica el certificado CA para la conexión SSL
     }
-    console.log("Conexion exitosa")
+});
 
-})
+conn.connect((error) => {
+    if (error) {
+        console.error(`Error al conectarse a la base de datos: ${error.message}`);
+        return;
+    }
+    console.log("Conexión exitosa a la base de datos MySQL");
+});
 
-process.on("SIGINT",()=>{
-    conn.end();
-    process.exit();
-})
+process.on("SIGINT", () => {
+    console.log("Cerrando conexión...");
+    conn.end((err) => {
+        if (err) {
+            console.error(`Error al cerrar la conexión: ${err.message}`);
+        } else {
+            console.log("Conexión cerrada. Saliendo...");
+            process.exit();
+        }
+    });
+});
 
-module.exports=conn;
+module.exports = conn;
